60ff09c1cfcbcb2eb741969d167e998805499b79
Yoshi Mans <kodivan@creekaitr.net>
2024-01-12 11:19:18 -0500

    wtf

--- game/menu.gd
+++ game/menu.gd
@@ -1,28 +1,53 @@
 extends Node
+var currentDoor = null
 
+@onready
+var menu = $Menu
+
+@export var doodoo : PackedScene
+@export var inMenu = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	print('fuc')
+	print('NEVA TOO MUCH!!!! NEVA TOO MUCH!!!! NEVA TOO MUCH!!!! NEVA TOO MUCH!!!')
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
-
+	if Input.is_action_just_pressed("ui_up") and inMenu:
+		print('yeah yeah yeah eyah')
+		print(currentDoor)
+		if currentDoor == 'tutorial':
+			await get_tree().create_timer(1).timeout
+			var newScee = doodoo.instantiate()
+			remove_child(menu)
+			add_child(newScee)
+			$Player.position = Vector2(60, 10)
+			
+			inMenu = false
+			
+			
 
 
 
 func _on_tutorial_door_body_entered(body):
-	print(body)
+	currentDoor = 'tutorial'
 	$Player/ColorRect.color = Color(0, 1, 1, 1)
 
 
 func _on_game_door_body_entered(body):
-	print(body)
+	currentDoor = 'game'
 	$Player/ColorRect.color = Color(1, 1, 0, 1)
 
 
 func _on_game_door_body_exited(body):
 	if body == $Player:
+		currentDoor = ''
 		$Player/ColorRect.color = Color(0.949, 0, 0.467, 1)
+
+
+func _on_player_death():
+	if ($GameMapNode):
+		remove_child($GameMapNode)
+		add_child(menu)
+		inMenu = true
--- game/player.gd
+++ game/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+signal death
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -1100.0
@@ -10,19 +11,37 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 func _physics_process(delta):
 	# Add the gravity.
+	var newGravity = gravity if not is_on_wall() else gravity / 2
+	
 	if not is_on_floor():
-		velocity.y += gravity * delta
+		velocity.y += newGravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("ui_accept"):
+		if is_on_floor():
+			velocity.y = JUMP_VELOCITY
+		elif is_on_wall() or is_on_wall_only():
+			velocity.y = 0
+			
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("ui_left", "ui_right")
+	
+	var newSpeed = SPEED * 2 if Input.is_action_pressed("shift") else SPEED
+	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = direction * newSpeed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, newSpeed)
 
 	move_and_slide()
+	
+	if not get_owner().inMenu:
+		get_owner().get_node('Camera2D').position = position
+
+
+func _on_death():
+	print('vamp anthem! 2')
+	position = Vector2(939, 402)
+	get_owner().get_node('Camera2D').position = Vector2(960, 540)
