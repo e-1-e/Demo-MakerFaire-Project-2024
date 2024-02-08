05fe9b000eed277c2888ab8355328335b3b6126d
Yoshi Mans <kodivan@creekaitr.net>
2024-02-08 15:24:45 -0500

    AHHH

--- game/Level 1.gd
+++ game/Level 1.gd
@@ -4,13 +4,22 @@ extends Node
 
 var projectileBlocks = []
 func wake():
-	#Detect up projectile tiles using tile at atlas coord (2, 3).
+	#Detect the TileMap tiles using tile at atlas coord (2, 3).
 	for i in $TileMap.get_used_cells(0):
 		if $TileMap.get_cell_atlas_coords(0, i) == Vector2i(2, 3):
 			projectileBlocks.push_front(i)
 	print(projectileBlocks)
 	
 	#Next, add the functionality for the projectile tiles.
+	while true:
+		for i in projectileBlocks:
+			var newProject = projecty.instantiate()
+			add_child(newProject)
+			
+			newProject.position = $TileMap.to_global($TileMap.map_to_local(i)) + Vector2(0, 70)
+			newProject.testProp = true
+			newProject.constantTravel(Vector2(0, 10))
+		await get_tree().create_timer(5).timeout
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
--- game/projectile.gd
+++ game/projectile.gd
@@ -2,6 +2,36 @@ extends Control
 
 @export var testProp = false
 
+#Only use if you just want the projectile travelling a certain direction. lmao
+func constantTravel(direction):
+	self.get_node('HeatSeeker').body_entered.connect(func (x):
+		#explosion whatever
+		if x.name == 'Player' and self.testProp:
+			self.testProp = false
+			x.changeHealth(1, 'hit by projectile 💀')
+			x.impulse(150, self.position.x < x.position.x, 10)
+			
+			var luhT = self.create_tween()
+			luhT.tween_property(self, 'modulate', Color(0, 0, 0, 0), 0.25)
+			luhT.tween_callback(self.queue_free)
+		elif x.name == 'TileMap':
+			self.testProp = false
+			var luhT = self.create_tween()
+			luhT.tween_property(self, 'modulate', Color(0, 0, 0, 0), 1)
+			luhT.tween_callback(self.queue_free)
+	)
+	
+	if self != null and get_tree() and self.testProp == true:
+		print("YOU CAN GET WHATCHU WANT")
+		while self:
+			self.position += direction
+			
+			if not get_tree(): return null
+			if not self.testProp: return null
+			await get_tree().create_timer(0.001).timeout
+			print('CHOSPTICK CAME WITH A LARGE LO MEIN')
+			if not is_instance_valid(self): return null
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
